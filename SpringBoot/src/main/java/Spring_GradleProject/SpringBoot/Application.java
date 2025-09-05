0ac391e29a7f305c1a9ed1c1cc0dc838ba69791d
Simon <sai@gmail.com>
2025-09-05 12:34:04 -0400

    5rth commit

--- SpringBoot/src/main/java/Spring_GradleProject/SpringBoot/Application.java
+++ SpringBoot/src/main/java/Spring_GradleProject/SpringBoot/Application.java
@@ -13,5 +13,6 @@ public class Application {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Hello");
 	}
 }
